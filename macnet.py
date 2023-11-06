a1933131d1859862d2f37b7c2ec9a0085640d9c8
ERC <149953300+ERC-Innovations@users.noreply.github.com>
2023-11-06 15:43:24 -0500

    Update macnet.py

--- macnet.py
+++ macnet.py
@@ -3,4 +3,4 @@ import sys
 import string
 url = "https://raw.githubusercontent.com/macnet-packages/macnet-packages/main/" + sys.argv[1]
 r = requests.get(url, allow_redirects=True)
-open(url.split("/")[-1], 'wb').write(r.content)
+open(url.split("/")[-1], 'wb').write(f"/opt/macnet/bin{r.content}")
