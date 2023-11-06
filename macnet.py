393c19e35de067e43e7cd0d6aee411cfbdd210bf
ERC <149953300+ERC-Innovations@users.noreply.github.com>
2023-11-06 14:14:28 -0500

    Add files via upload

---
+++ macnet.py
@@ -0,0 +1,6 @@
+import requests
+import sys
+import string
+url = "https://raw.githubusercontent.com/macnet-packages/macnet-packages/main/" + sys.argv[1]
+r = requests.get(url, allow_redirects=True)
+open(url.split("/")[-1], 'wb').write(r.content)
