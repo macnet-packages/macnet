44418ed39bbbdce7387c121351d700f5e657bbb9
ERC <149953300+ERC-Innovations@users.noreply.github.com>
2023-11-27 07:53:31 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
-    description='The MacNet Python Package Manager.',
+    description='The MacNet Package Manager.',
 )
 
 open(f"{os.getenv('HOME')}/.zshrc", "a").write("alias macnet='python3 -m macnet'")
